63fb08fa3f1dd8f12324390aff91f6c85402dcec
Augustin Anders <121796762+augustinanders@users.noreply.github.com>
2023-03-09 20:27:51 +0100

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import "./Card.css";
+import Tag from "./Tag";
+import Button from "./Button";
+
+function Card({ user }) {
+  return (
+    <article className="card">
+      <h2>{user.name}</h2>
+      <ul className="card__taglist">
+        {user.roles.map((role) => {
+          return <Tag key={role} tag={role} />;
+        })}
+      </ul>
+      <p>{user.about}</p>
+      <Button>edit</Button>
+      <Button buttonType="danger">invoice</Button>
+    </article>
+  );
+}
+
+export default Card;
